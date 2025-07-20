26cdb41945b3f7b8f0bd2caae48757660abfc7a2
agxmeister <agxmeister@gmail.com>
2025-07-20 21:03:44 +0300

    Parse the services configuration file at the application's entry point.

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,16 @@ import {StdioServerTransport} from "@modelcontextprotocol/sdk/server/stdio.js";
 import {getServer} from "./server";
 import minimist from 'minimist';
 import path from 'path';
+import {getServices} from "./utils";
 
 dotenv.config();
 const args = minimist(process.argv.slice(2));
 
 (async () => {
-    const server = await getServer(args.services
+    const server = await getServer(await getServices(args.services
         ? path.resolve(args.services)
         : undefined
-    );
+    ));
     const transport = new StdioServerTransport();
     await server.connect(transport);
 })();
--- src/server/utils.ts
+++ src/server/utils.ts
@@ -1,14 +1,14 @@
 import {ServerBuilder} from "./ServerBuilder";
 import {CallApiEndpoint, GetApiEndpointDetails, GetApiEndpoints} from "./tools";
-import {getServices} from "../utils";
+import {Service} from "../types";
 
-export const getServer = async (configPath?: string) => {
+export const getServer = async (services: Service[]) => {
     const builder = new ServerBuilder([
         GetApiEndpoints,
         GetApiEndpointDetails,
         CallApiEndpoint,
     ]);
-    for (const service of await getServices(configPath)) {
+    for (const service of services) {
         builder.addService(service);
     }
     return builder.build();
