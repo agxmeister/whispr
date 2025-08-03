3b8516c233385de5d06d21e143a93cf338995f52
agxmeister <agxmeister@gmail.com>
2025-08-03 12:33:48 +0300

    Do not display API endpoint descriptions if they are empty.

--- src/server/tools/GetApiEndpoints.ts
+++ src/server/tools/GetApiEndpoints.ts
@@ -1,6 +1,6 @@
 import {McpServer} from "@modelcontextprotocol/sdk/server/mcp.js";
 import {Service} from "../../types";
-import {Tool} from "./types";
+import {Tool, OpenApiEndpoint} from "./types";
 import {getNames, getDescriptions, getOpenApiEndpoints} from "./utils";
 
 export const GetApiEndpoints: Tool = {
@@ -12,12 +12,16 @@ export const GetApiEndpoints: Tool = {
                 content: [{
                     type: "text",
                     text: (await getOpenApiEndpoints(service.api.specification))
-                        .map(({method, path, details}) =>
-                            `${method} ${path} - ${details.summary ?? details.description}`,
-                        )
+                        .map((openApiEndpoint) => getApiEndpointDescription(openApiEndpoint))
                         .join("\n\n"),
                 }]
             })
         );
     }
 }
+
+const getApiEndpointDescription = ({method, path, details}: OpenApiEndpoint): string => {
+    const endpoint = `${method} ${path}`;
+    const explanation = details.summary ?? details.description;
+    return explanation ? `${endpoint} - ${explanation}` : endpoint;
+};
